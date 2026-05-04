186dbcd37c96c1d3bbdc71edf333150335479293
Yohai Ribeiro <wasnelpower@gmail.com>
2026-05-04 08:41:17 -0300

    Delete kore-site directory

--- kore-site/script.js
+++
@@ -1,6 +0,0 @@
-const botao = document.querySelector('.menu-hamburger');
-const menu = document.querySelector('.menu-mobile');
-
-botao.addEventListener('click', () => {
-    menu.classList.toggle('active');
-});
